e422ab4200257dd8f08072e7de483e8eeb9d16aa
Ankit Singh <ankitas90@gmail.com>
2017-09-20 22:30:45 +0530

    api key authentication on question show

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,8 +1,25 @@
 class Api::QuestionsController < ApplicationController
 
+	before_filter :authenticate_key
+	
 	def show
 	  @question = Question.find(params[:id])
 	  render json: @question.to_json
 	end
+
+	private
+
+		def question_params
+			params.require(:questions).permit(:id, :title, :private, :user_id)
+		end
+
+		def has_valid_api_key?
+			key = Tenant.find_by(api_key: request.headers['HTTP_X_API_KEY'])
+			return key.present?
+		end
+
+		def authenticate_key
+			return head :forbidden unless has_valid_api_key?
+		end
 
 end
